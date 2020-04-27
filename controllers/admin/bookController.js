146a6533c35b9abf74ed3cfc09cee1e4005865ea
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-27 23:27:17 +0200

    add book list & create

---
+++ controllers/admin/bookController.js
@@ -0,0 +1,48 @@
+const Book = require('../../models/Book');
+const Author = require('../../models/Author');
+const Category = require('../../models/Category');
+
+let views = "admin/book"
+
+exports.index = async (req, res) => {
+    try {
+        let books = await Book.find({});
+        res.render(`${views}/index`, {books});
+    } catch (e) {
+        res.json({err: e})
+    }
+}
+
+exports.create = async (req, res) => {
+    try {
+        let authors = await Author.find({});
+        let categories = await Category.find({});
+        res.render(`${views}/create`, {authors, categories});
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+
+}
+
+exports.store = async (req, res) => {
+    let {name, author, category} = req.body;
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let image = req.files.image;
+    let imgName = image.md5 + image.name;
+
+    try {
+        await image.mv(`public/upload/books/${imgName}`);
+        let book = await Book.create({
+            name: name,
+            author: author,
+            category: category,
+            image: imgName,
+        });
+        res.redirect("/admin/books");
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
