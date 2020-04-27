ff5e8a2b0c31f8b16ab87603cc7f1f689a634d95
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-04-27 02:17:09 +0200

    create book_rating model

---
+++ models/Book_rating.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+    rate:{
+        type: {
+            type: Number,
+            required: true
+        }
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+}, { timestamps: true });
+
+
+const Book_rating = mongoose.model('Book_Rating', schema);
+module.exports = Book_rating;
