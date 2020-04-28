f86e6573d2b86c743b2599eabe4e591bf477a2b6
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-28 17:45:29 +0200

    edit category in admin

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -7,7 +7,7 @@ exports.index = async (req, res) => {
         let categories = await Category.find({});
         res.render(`${views}/index`, {categories: categories});
     } catch (e) {
-        res.json({err:e})
+        res.json({err: e})
     }
 }
 
@@ -23,4 +23,32 @@ exports.store = async (req, res) => {
     } catch (err) {
         throw err;
     }
+}
+
+exports.edit = async (req, res) => {
+    try{
+        let {id} = req.params;
+        let category = await Category.findOne({_id: id})
+        res.render(`${views}/edit`, {category});
+    }catch (e) {
+        res.status(500).err(e)
+    }
+
+}
+
+exports.update = async (req, res) => {
+    let {id} = req.params;
+    let {name} = req.body;
+    try{
+        let category = await Category.findByIdAndUpdate(id, {
+            $set:
+                {
+                    name: name
+                }
+        }, {new: true})
+        res.redirect("/admin/categories");
+    }catch (e) {
+        res.status(500).err(e)
+    }
+
 }
