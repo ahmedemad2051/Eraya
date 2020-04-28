a3a271fd4304788f321df615ceb0ccd8773b0542
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-04-28 02:04:37 +0200

    update category books page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,27 @@
+const CategoryModel = require('../models/Category')
+const BookModel = require('../models/Book')
+
 exports.home = (req, res) => {
     res.render('front/home');
 }
 
-exports.categories = (req, res) => {
-    res.render('front/categories');
+exports.categories = async (req, res, next) => {
+    try{
+        const categories = await CategoryModel.find({}).populate('admin')
+        return res.render('front/categories', {categories: categories});
+
+    }catch(err){
+        next(err)
+    }
+
 }
 
-exports.categoryBooks = (req, res) => {
-    res.render('front/category_books');
+exports.categoryBooks = async (req, res, next) => {
+    try{
+        const books = await BookModel.find({}).populate('category').populate('author')
+        return res.render('front/category_books', {books: books});
+
+    }catch(err){
+        next(err)
+    }
 }
