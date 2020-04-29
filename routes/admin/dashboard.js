1b832fdbc13f722ebb5043be1fa55682eb53038f
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-29 18:11:15 +0200

    delete category in admin

--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -11,6 +11,7 @@ router.get('/categories/create', categoryController.create);
 router.post('/categories/create', categoryController.store);
 router.get('/categories/:id/edit', categoryController.edit);
 router.post('/categories/:id/edit', categoryController.update);
+router.post('/categories/:id/delete', categoryController.destroy);
 
 
 router.get('/authors', authorController.index);
@@ -18,11 +19,14 @@ router.get('/authors/create', authorController.create);
 router.post('/authors/create', authorController.store);
 router.get('/authors/:id/edit', authorController.edit);
 router.post('/authors/:id/edit', authorController.update);
+router.post('/authors/:id/delete', authorController.destroy);
+
 
 router.get('/books', bookController.index);
 router.get('/books/create', bookController.create);
 router.post('/books/create', bookController.store);
 router.get('/books/:id/edit', bookController.edit);
 router.post('/books/:id/edit', bookController.update);
+router.post('/books/:id/delete', bookController.destroy);
 
 module.exports = router;
