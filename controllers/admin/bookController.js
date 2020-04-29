6d3487679be9e14e029c87c1d02ffb269702dd11
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-29 18:11:52 +0200

    delete book in admin

--- controllers/admin/bookController.js
+++ controllers/admin/bookController.js
@@ -92,3 +92,13 @@ exports.update = async (req, res) => {
     }
 
 }
+
+exports.destroy = async (req, res) => {
+    let {id} = req.params;
+    try {
+        await Book.findByIdAndRemove(id)
+        res.status(200).json({"msg": 'success'});
+    } catch (e) {
+        res.status(500).err(e)
+    }
+}
