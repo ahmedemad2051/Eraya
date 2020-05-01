84eb4aacc7fcbca6dc6373cc79e3485df7213d02
mohamedsalama11022 <mohamed.helmy11022@gmail.com>
2020-05-01 22:44:48 +0200

    Registeration

--- controllers/authentication/signUpController.js
+++ controllers/authentication/signUpController.js
@@ -15,17 +15,56 @@ exports.register = async(req, res, next) => {
         })
           try{
             const newOne = await newUser.save()
-            console.log("succededddddddddddddddddddddddddddddddd")
+            console.log("Registered Successfully!")
             res.redirect('/signUp')
           }  
             catch(err){
-                console.log("errrrrrrrrrrrrrrrrrrrror")
-                console.log(err.code)
+                validateForm(fName, lName, email, password, confirmPassword)
                 res.redirect('/signUp')
             }
-       
-     
-        
-
     
+}
+
+function validateForm(fName, lName, email, password, confirmPassword) {
+
+  if (!fName) {
+      // flashMessage
+      console.log("First Name can not be empty")
+  }
+  
+  if (!lName) {
+      // flashMessage
+      console.log("Last Name can not be empty")
+  }
+
+  if (!password) {
+      // flashMessage
+      console.log("Password can not be empty")
+  } else if (password.length < 8) {
+      console.log("Password must be at least 8 characters")
+      // passwordError.innerHTML = "Password must be at least 8 characters";
+  } 
+      if (confirmPassword != password) {
+          console.log("Password does not match")
+          // confirmPasswordError.innerHTML = "Password does not match"
+      }
+
+      if (!validateEmail(email)) {
+        console.log("Not a valid email format")
+        // emailError.innerHTML = "Not a valid email format";
+      }
+      User.find({'email': email}, function(err, user){
+        if(user.length != 0){
+          if(user[0].email){
+            console.log("this email alresdy exists "+ email)
+          }
+        }
+      }) 
+
+}
+
+function validateEmail(email){
+  var re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+  return re.test(String(email).toLowerCase())
+
 }
