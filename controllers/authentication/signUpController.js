811353c5f0ff1cf4e25e2434c81cef30d7598eb0
mohamedsalama11022 <mohamed.helmy11022@gmail.com>
2020-05-03 04:27:16 +0200

    store logged in user in session and change backEnd validation method

--- controllers/authentication/signUpController.js
+++ controllers/authentication/signUpController.js
@@ -1,6 +1,7 @@
 const User = require('../../models/User')
 
 exports.signUp = (req, res) => {
+  
     res.render('front/signUp')
 }
 
@@ -13,58 +14,87 @@ exports.register = async(req, res, next) => {
             email,
             password,
         })
+        let validation = validateForm(fName, lName, email, password, confirmPassword)
+        if(validation[0]){
           try{
-            const newOne = await newUser.save()
-            console.log("Registered Successfully!")
-            res.redirect('/signUp')
+            //validateExistance(email) returns true if the email in exist
+            let user = await User.findOne({email: req.body.email})
+            if(!user){
+              const newOne = await newUser.save()
+              console.log("Registered Successfully!")
+              res.redirect('/home')
+            }else{
+                console.log("This email alresdy exists")
+                req.flash("info", "This email alresdy exists")
+                res.redirect('/signup')
+            }  
           }  
             catch(err){
-                validateForm(fName, lName, email, password, confirmPassword)
                 res.redirect('/signUp')
-            }
+          }
+        }else{
+          for(var i=0; i<validation.length; i++){
+            req.flash("info", validation[i])
+            console.log(validation[i])
+          }
+          res.redirect('/signup')
+        }
     
 }
 
 function validateForm(fName, lName, email, password, confirmPassword) {
-
+  let isValid = []
+  isValid[0] = true
   if (!fName) {
-      // flashMessage
-      console.log("First Name can not be empty")
+    isValid[0] = false
+    isValid.push("First Name is required")
   }
   
   if (!lName) {
-      // flashMessage
-      console.log("Last Name can not be empty")
+    isValid[0] = false
+    isValid.push("Last Name is required")
+
   }
 
   if (!password) {
-      // flashMessage
-      console.log("Password can not be empty")
+    isValid[0] = false
+    isValid.push("Password is required")
+      
   } else if (password.length < 8) {
-      console.log("Password must be at least 8 characters")
-      // passwordError.innerHTML = "Password must be at least 8 characters";
+    isValid[0] = false
+    isValid.push("Password must be at least 8 characters")
+
   } 
       if (confirmPassword != password) {
-          console.log("Password does not match")
-          // confirmPasswordError.innerHTML = "Password does not match"
+        isValid[0] = false
+        isValid.push("Passwords doesn't match")
       }
 
       if (!validateEmail(email)) {
-        console.log("Not a valid email format")
-        // emailError.innerHTML = "Not a valid email format";
+        isValid[0] = false
+        isValid.push("not a valid email format")
       }
-      User.find({'email': email}, function(err, user){
-        if(user.length != 0){
-          if(user[0].email){
-            console.log("this email alresdy exists "+ email)
-          }
-        }
-      }) 
-
+      return isValid
 }
 
 function validateEmail(email){
   var re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
   return re.test(String(email).toLowerCase())
 
+}
+// exports.login = async(req, res, next) => {
+// let user = await User.findOne({email: email})
+ validateExistance = async (email) => {
+   try{
+      let user = await User.findOne({email: email})
+      if(user){
+        return true
+      }else{
+        return false
+      }
+     
+   }catch(err){
+    //  return false
+    console.log(err)
+   }
 }
